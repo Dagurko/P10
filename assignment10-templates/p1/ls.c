ed9e138ad9abea6f7ed579b6bba4e902aac5657d
Dagurko <112619676+Dagurko@users.noreply.github.com>
2024-03-25 18:27:58 +0000

    Almost finish p1 fix need to fix symlink

--- assignment10-templates/p1/ls.c
+++ assignment10-templates/p1/ls.c
@@ -1,7 +1,7 @@
 /* 
  * Group number (on canvas): xx
- * Student 1 name: xx 
- * Student 2 name: xx 
+ * Student 1 name: Dagur Kári Ólafsson
+ * Student 2 name: Jónas Ingi Þórisson
  */
 
 #include "ls.h"
@@ -18,9 +18,48 @@
 
 
 int list(const char* path, int recursive)
-{
-	(void) path;
-	(void) recursive;
+{	
+	DIR* dir = opendir(path);
+	if (dir == NULL) {
+		fprintf(stderr, "Error opening directory %s: %s\n", path, strerror(errno));
+		return 1;
+	}
 
-	return -1;
+	
+	struct dirent* entry;
+	while ((entry = readdir(dir)) != NULL) {
+		if (entry->d_name[0] == '.' || strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+
+		char full_path[1024];
+		snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
+
+		struct stat info;
+		if (lstat(full_path, &info) < 0) {
+        	perror("lstat");
+        	exit(EXIT_FAILURE);
+    	}
+
+		char* typestr = "";
+		if (S_ISDIR(info.st_mode)) {
+			typestr = "/";
+		} else if (S_ISFIFO(info.st_mode)) {
+			typestr = "|";
+		} else if (S_ISREG(info.st_mode) && (info.st_mode & (S_IXUSR))) {
+			typestr = "*";
+    	} else if (S_ISLNK(info.st_mode)) {
+			typestr = "->";
+		}
+		_printLine(info.st_size, full_path, typestr);
+
+		
+		if (S_ISDIR(info.st_mode) && recursive) {
+			list(full_path, recursive);
+		}
+		
+	}
+
+	closedir(dir);
+	return 0;
 }
